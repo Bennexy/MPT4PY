faafca576495ef92a7888e159fe7fbcce62b2c41
Bennexy <74596167+Bennexy@users.noreply.github.com>
2021-03-23 23:35:57 +0100

    asdf

--- server/server-main.py
+++ server/server-main.py
@@ -3,16 +3,17 @@ import socket
 HOST = "192.168.0.164"
 PORT = 5555
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind((HOST, PORT))
-s.listen()
-conn, addr = s.accept()
-print('Connected by', addr)
+
+print("starting up")
 try:
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind((HOST, PORT))
+    s.listen()
+    conn, addr = s.accept()
+    print('Connected by', addr)
     while True:
         data = conn.recv(1024)
-        if not data:
-            break
+
         conn.sendall(data)
 except KeyboardInterrupt:
     print("shutting down")
